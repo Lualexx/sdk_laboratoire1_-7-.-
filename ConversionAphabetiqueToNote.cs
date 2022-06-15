8a8448f106a09a1fb9cee1eceedfa4a19a0ba218
cg2223 <98047195+cg2223@users.noreply.github.com>
2022-06-15 19:32:51 -0400

    Add files via upload

---
+++ ConversionAphabetiqueToNote.cs
@@ -0,0 +1,67 @@
+﻿namespace LibrarieConversionNote
+{
+    public class ConversionAphabetiqueToNote
+    {
+        static void Main(string[] arg)
+
+        {
+
+
+
+            
+            Console.WriteLine(input);
+            String str = "C";
+            Console.WriteLine("Old String: " + str);
+
+            // chain together
+            str = str.Replace('C', 'Y').Replace('Y', 'Z').Replace('Z', 'D');
+            Console.WriteLine("New string: " + str);
+            Console.ReadLine(); 
+
+
+
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+    /*
+    String s = new String('C', 'Do');
+    Console.WriteLine("The initial string: '{0}'", s);
+    s = s.Replace('C', 'X').Replace('X', 'Y').Replace('Y', 'Do');
+    Console.WriteLine("The final string: '{0}'", s);
+
+    
+    char note1 = "C";
+    string changement = char.Parse(note1);
+{
+    Console.WriteLine("Ecris un note");
+    String Note = Parse(Console.ReadLine());
+    Console.WriteLine(Note1);
+
+
+
+
+    string Note = "C";
+    Console.WriteLine("Original: {0}", Note);
+    string remplacer = Note.Replace("C", "DO");
+    Console.WriteLine("Remplacer: {0}", remplacer);
+// O caracteres
+
+   
+
+
+}
+    }
+}
+     */
