cf2f01bc818431270ec852db8959b5a6c145d680
cg2223 <98047195+cg2223@users.noreply.github.com>
2022-06-15 19:29:19 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,16 +1,27 @@
-﻿
-Console.WriteLine("\t\t\t\t\t\tConvertiseur alphabetique! ");
-Console.WriteLine("\n\t\t\t\t\t\t\t A = La ");
-Console.WriteLine("\n\t\t\t\t\t\t\t B = Si ");
-Console.WriteLine("\n\t\t\t\t\t\t\t C = Do ");
-Console.WriteLine("\n\t\t\t\t\t\t\t D = Re ");
-Console.WriteLine("\n\t\t\t\t\t\t\t E = Mi ");
-Console.WriteLine("\n\t\t\t\t\t\t\t F = Fa ");
-Console.WriteLine("\n\t\t\t\t\t\t\t G = Sol ");
-Console.Write("\n\t\t\t\t\t\tEcrire votre choix: ");
-string choix = Console.ReadLine();
-var input = Console.ReadLine();
-Console.WriteLine("\t\t\t\t\t\tTu as choisi: " + choix);
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-
-Console.WriteLine(input);
+namespace _1.SDK_Laboratoire1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t\t\tConvertiseur alphabetique! ");
+            Console.WriteLine("\n\t\t\t\t\t\t\t A = La ");
+            Console.WriteLine("\n\t\t\t\t\t\t\t B = Si ");
+            Console.WriteLine("\n\t\t\t\t\t\t\t C = Do ");
+            Console.WriteLine("\n\t\t\t\t\t\t\t D = Re ");
+            Console.WriteLine("\n\t\t\t\t\t\t\t E = Mi ");
+            Console.WriteLine("\n\t\t\t\t\t\t\t F = Fa ");
+            Console.WriteLine("\n\t\t\t\t\t\t\t G = Sol ");
+            Console.Write("\n\t\t\t\t\t\tEcrire votre choix: ");
+            string choix = Console.ReadLine();
+            var input = Console.ReadLine();
+            Console.WriteLine("\t\t\t\t\t\tTu as choisi: " + choix);
+        }
+    }
+}
