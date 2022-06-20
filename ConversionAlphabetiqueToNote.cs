c65d7e9681854fbdaaf73d0d77b24a76e18508de
Lualex <102312892+Lualexx@users.noreply.github.com>
2022-06-20 10:49:51 -0400

    Add files via upload

---
+++ ConversionAlphabetiqueToNote.cs
@@ -0,0 +1,47 @@
+﻿namespace ConversionAphabetiqueToNote
+{
+    public class ConversionAphabetiqueToNote
+    {
+        public string Format(string str)
+
+        {
+            string strReturnValue = string.Empty;
+            switch (str.ToLowerInvariant())
+            {
+                case "a":
+                    strReturnValue = "La";
+                        break;
+                case "b":
+                    strReturnValue = "Si";
+                        break;
+                case "c":
+                    strReturnValue = "Do";
+                        break;
+                case "d":
+                    strReturnValue = "Re";
+                        break;
+                case "e":
+                    strReturnValue = "Mi";
+                        break;
+                case "f":
+                    strReturnValue = "Fa";
+                        break;
+                case "g":
+                    strReturnValue = "Sol";
+                        break;
+
+            }
+            return strReturnValue;
+            
+           
+        }
+        public string NoReturn(string str)
+        {
+            return "";
+        }
+    }
+}
+
+
+
+
