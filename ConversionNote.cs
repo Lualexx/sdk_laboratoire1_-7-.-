3a7e4595f8eda5897cd24e8e9b346e5e7f221067
cg2223 <98047195+cg2223@users.noreply.github.com>
2022-06-20 21:07:00 -0400

    SDK_Laboratoire_1

---
+++ ConversionNote.cs
@@ -0,0 +1,72 @@
+﻿namespace ConversionNote
+{
+
+    public class ConversionNote
+    {
+
+        public string Formateur(string str)
+        {
+            string strReturnValue = string.Empty;
+            switch (str.ToLowerInvariant())
+            {
+                case "a";
+                    strReturnValue = "LA";
+                    break;
+                case "b";
+                    strReturnValue = "SI";
+                    break;
+                case "c";
+                    strReturnValue = "DO";
+                    break;
+                case "d";
+                    strReturnValue = "RE";
+                    break;
+                case "e";
+                    strReturnValue = "MI";
+                    break;
+                case "f";
+                    strReturnValue = "FA";
+                    break;
+                case "g"
+                    strReturnValue = "SOL";
+                    break;
+            }
+
+        }
+    }
+
+}
+
+
+
+
+/*
+String s = new String('C', 'Do');
+Console.WriteLine("The initial string: '{0}'", s);
+s = s.Replace('C', 'X').Replace('X', 'Y').Replace('Y', 'Do');
+Console.WriteLine("The final string: '{0}'", s);
+
+
+
+
+
+string Note = "C";
+Console.WriteLine("Original: {0}", Note);
+string remplacer = Note.Replace("C", "DO");
+Console.WriteLine("Remplacer: {0}", remplacer);
+// O caracteres
+
+
+ 
+            Console.WriteLine(input);
+            String str = "C";
+            Console.WriteLine("Old String: " + str);
+
+            // chain together
+            str = str.Replace('C', 'Y').Replace('Y', 'Z').Replace('Z', 'D');
+            Console.WriteLine("New string: " + str);
+            Console.ReadLine(); 
+
+
+
+ */
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using ConversionNote;
+
+while (true)
+{
+    Console.Write("conversion a faire :");
+    var input = Console.ReadLine();
+    if (input?.ToUpperInvariant() == "NOTE")
+    {
+        while (true)
+        {
+
+            Console.Write("convertiseur de note:");
+            ConsoleKeyInfo keyPress = Console.ReadKey(intercept: true);
+            string strValeurPresser = keyPress.KeyChar.ToString();
+            ConversionNote.ConversionNote cn = new ConversionNote.ConversionNote();
+            string note = cn.Formateur(strValeurPresser);
+            Console.WriteLine($"Note : {note}");
+
+        }
+    }
+
+
+
+
+}
+
+
+
+/*
+
+namespace _1.SDK_Laboratoire1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t\t\tConvertiseur alphabetique! ");
+            Console.WriteLine("\n\t\t\t\t\t\t\t A = La ");
+            Console.WriteLine("\n\t\t\t\t\t\t\t B = Si ");
+            Console.WriteLine("\n\t\t\t\t\t\t\t C = Do ");
+            Console.WriteLine("\n\t\t\t\t\t\t\t D = Re ");
+            Console.WriteLine("\n\t\t\t\t\t\t\t E = Mi ");
+            Console.WriteLine("\n\t\t\t\t\t\t\t F = Fa ");
+            Console.WriteLine("\n\t\t\t\t\t\t\t G = Sol ");
+            Console.Write("\n\t\t\t\t\t\tEcrire votre choix: ");
+            string choix = Console.ReadLine();
+            var input = Console.ReadLine();
+            Console.WriteLine("\t\t\t\t\t\tTu as choisi: " + choix);
+
+
+
+        }
+    }
+}
